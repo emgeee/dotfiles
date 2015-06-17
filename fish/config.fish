aafa282915a48586644a8e0f5fc315e59a311764
Matt Green <mgreen9@gmail.com>
2015-06-17 13:28:03 -0700

    hard code node v12.4

--- fish/config.fish
+++ fish/config.fish
@@ -4,18 +4,10 @@ set fish_path $HOME/.oh-my-fish
 # Path to your custom folder (default path is ~/.oh-my-fish/custom)
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
-# Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+### Begin custom fish customization
 
-Theme 'robbyrussell'
-Plugin 'theme'
-Plugin 'git'
-Plugin 'node'
-Plugin 'tmux'
-Plugin 'autojump'
-Plugin 'vi-mode'
-Plugin 'brew'
-
+# erase any default greeting message
+set --erase fish_greeting
 
 # PATH
 set -e fish_user_paths
@@ -48,4 +40,34 @@ function sudo_update_locate
   sudo /usr/libexec/locate.updatedb
 end
 
+# Configure nvm
+
+# Manually add env variables to start with v0.12.4 to increase startup time
+set -x NVM_BIN /Users/matt/.nvm/versions/node/v0.12.4/bin
+set -x NVM_DIR /Users/matt/.nvm
+set -x NVM_IOJS_ORG_MIRROR https://iojs.org/dist
+set -x NVM_IOJS_ORG_VERSION_LISTING https://iojs.org/dist/index.tab
+set -x NVM_NODEJS_ORG_MIRROR https://nodejs.org/dist
+set -x NVM_PATH /Users/matt/.nvm/versions/node/v0.12.4/lib/node
+set PATH /Users/matt/.nvm/versions/node/v0.12.4/bin $PATH
+
+set -x NVM_DIR ~/.nvm
 source ~/.config/fish/nvm-wrapper/nvm.fish
+
+## This is pretty slow but works
+# nvm use default 2>&1 >/dev/null
+
+### End custom fish config
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# Load fish plugins
+Theme 'robbyrussell'
+Plugin 'theme'
+Plugin 'osx'
+Plugin 'node'
+Plugin 'tmux'
+Plugin 'vi-mode'
+Plugin 'brew'
+
+
