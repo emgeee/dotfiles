816e1f33102f825df72b1899bca025be422ba7af
Matt Green <matt.green@fast.co>
2022-01-07 14:16:07 -0800

    tweak tree settings

--- nvim/lua/plugins/tree-config.lua
+++ nvim/lua/plugins/tree-config.lua
@@ -1,15 +1,20 @@
-
-require('nvim-tree').setup()
-
 -- File Tree config
 local key_mapper = require('utils.key_mapper')
 local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
-vim.g.nvim_tree_bindings = {
-{ key = "?", cb = tree_cb("toggle_help") },
+local list = {
+  { key = "?", cb = tree_cb("toggle_help") },
 }
+
+require('nvim-tree').setup({
+  auto_close = true,
+  open_on_setup = true,
+  disable_netrw = false,
+  view = {
+    mappings = {
+      list = list
+    }
+  }
+})
 key_mapper('n', '<F3>', ':NvimTreeToggle<CR>')
 key_mapper('n', '<F4>', ':NvimTreeFindFile<CR>')
-
--- We need to keep netrw enabled so :GBrowse will work!
-vim.g.nvim_tree_disable_netrw = 0
