a692745115234c6a3fc5f791e49f3e6173cac8ae
Matt Green <emgeee@users.noreply.github.com>
2025-05-15 07:32:33 -0700

    add netlify.fish

---
+++ fish/completions/netlify.fish
@@ -0,0 +1,4 @@
+# tabtab source for packages
+# uninstall by removing these lines
+# netlify completion:install
+[ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
