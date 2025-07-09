a8eb26801fdf35fafe2eb075f7142748ea6e0ac8
Matt Green <emgeee@users.noreply.github.com>
2025-07-09 15:21:55 -0700

    update

--- nvim/lua/plugins/filetree.lua
+++ nvim/lua/plugins/filetree.lua
@@ -93,9 +93,8 @@ return {
 				pattern = "NvimTree*",
 				callback = function()
 					local api = require("nvim-tree.api")
-					local view = require("nvim-tree.view")
 
-					if not view.is_visible() then
+					if not api.tree.is_visible() then
 						api.tree.open()
 					end
 				end,
